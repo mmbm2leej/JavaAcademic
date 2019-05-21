edc8820c3559ddcb02be26e4874d29d605d919b0
Purple Squirrel <33865731+mmbm2leej@users.noreply.github.com>
2019-05-21 13:04:55 -0400

    Create Node.java

---
+++ Node.java
@@ -0,0 +1,13 @@
+class Node {
+    public String name;
+    public Node next;
+    Node( ) {
+        name = "";
+        next = null;
+    }
+    Node(String s) {  
+    	/*set the name */
+    	name = s;
+    	next = null;
+    }
+}
